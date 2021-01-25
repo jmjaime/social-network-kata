7e622e5a065cf6b50aad528c7c0e036b9997dbed
Juan Manuel Jaime <jjaime@yopapp.me>
2021-01-25 15:08:52 -0300

    Initial commit

---
+++ src/main/kotlin/kwikemart/socialnetwork/SocialNetworkApplication.kt
@@ -0,0 +1,11 @@
+package kwikemart.socialnetwork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialNetworkApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialNetworkApplication>(*args)
+}
---
+++ src/test/kotlin/kwikemart/socialnetwork/SocialNetworkApplicationTests.kt
@@ -0,0 +1,13 @@
+package kwikemart.socialnetwork
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
